83a49ff97bed7003ca196d1214a677cd4f881862
sredamar <92477629+sredamar@users.noreply.github.com>
2022-01-06 20:02:38 +0300

    Add files via upload

---
+++ itc_pos_neg_separator_lst.cpp
@@ -0,0 +1,7 @@
+#include "middle_list.h"
+void itc_pos_neg_separator_lst(const vector <int> &lst, vector <int> &lst1, vector <int> &lst2, vector <int> &lst3) {
+    for (int i = 0; i < lst.size(); i++) {
+        if (lst[i] < 0) lst1.push_back(lst[i]);
+        else if (lst[i] == 0) lst2.push_back(lst[i]);
+        else lst3.push_back(lst[i]);}
+}
---
+++ itc_rmstrspc.cpp
@@ -0,0 +1,5 @@
+#include "middle_list.h"
+string itc_rmstrspc(string str) {
+    string n = "";
+    for (int i = 0; str[i] != '\0'; i++) if (str[i] != 32) n += str[i];
+return n;}
