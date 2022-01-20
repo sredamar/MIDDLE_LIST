d5123f277170f9238c8912ec737b7d089f7dac5e
sredamar <92477629+sredamar@users.noreply.github.com>
2022-01-20 11:55:14 +0300

    #6

--- itc_sum_even_lst.cpp
+++ itc_sum_even_lst.cpp
@@ -1,7 +1,5 @@
 #include "middle_list.h"
 long itc_sum_even_lst(const vector <int> &lst) {
-    if (lst.size() != 0) {
     long sum = 0;
     for (int i = 0; i < lst.size(); i+=2) sum += lst[i];
-    return sum;}
-return 0;}
+return sum;}
