8091ba065352d7ec056468076876fc29dc4c6f16
sredamar <92477629+sredamar@users.noreply.github.com>
2022-01-20 18:16:17 +0300

    #2

--- itc_join.cpp
+++ itc_join.cpp
@@ -1,9 +1,9 @@
 #include "middle_list.h"
 string itc_join(vector <char> lst, string sep){
-    string n = "";
-    if (lst.size() > 0){
-    for(int i = 0; i < (lst.size() - 1); i++){
+    string n ="";
+    if(lst.size() > 0){
+    for(long long i =0; i < lst.size()-1; i++){
         n += lst[i];
         n += sep;}
-    n += lst[lst.size() - 1];}
+    n += lst[lst.size()-1];}
 return n;}
