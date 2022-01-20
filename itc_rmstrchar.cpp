eb09bfc52a364e96730b9c48dd98c9085b950bab
sredamar <92477629+sredamar@users.noreply.github.com>
2022-01-20 18:18:17 +0300

    #4

--- itc_rmstrchar.cpp
+++ itc_rmstrchar.cpp
@@ -1,3 +1,9 @@
 #include "middle_list.h"
 string itc_rmstrchar(string str, string l) {
-return 0;}
+    string n = "";
+    int b  = 0;
+    for (long long i = 0; str[i] != '\0'; i++) {
+        for(long long j = 0 ; l[j] != '\0'; j++) if (l[j] == str[i]) b  = 1;
+    if (b == 0) n += str[i];
+    b = 0;}
+return n;}
