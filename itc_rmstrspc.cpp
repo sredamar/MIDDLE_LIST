24d5850d04c5137aa25c098fee54c466eb1630a4
sredamar <92477629+sredamar@users.noreply.github.com>
2022-01-20 11:49:02 +0300

    #3

--- itc_rmstrspc.cpp
+++ itc_rmstrspc.cpp
@@ -1,7 +1,5 @@
 #include "middle_list.h"
-string itc_rmstrspc(string str) {
-    if (itc_len(str) != 0) {
+string itc_rmstrspc(string str){
     string n = "";
-    for (int i = 0; str[i] != '\0'; i++) if (str[i] != 32) n += str[i];
+    for(long long i = 0; i < itc_len(str); i++) if (str[i] != 32) n += str[i];
 return n;}
-}
