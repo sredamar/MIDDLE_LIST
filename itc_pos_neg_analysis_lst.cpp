7e70697f8f9b7d90b3239f8afe8cea0367eea2f5
sredamar <92477629+sredamar@users.noreply.github.com>
2022-01-20 12:17:35 +0300

    Update itc_pos_neg_analysis_lst.cpp

--- itc_pos_neg_analysis_lst.cpp
+++ itc_pos_neg_analysis_lst.cpp
@@ -1,13 +1,13 @@
 #include "middle_list.h"
 void itc_pos_neg_analysis_lst(const vector <int> &lst) {
-    setlocale(LC_ALL, "rus");
+    setlocale(LC_ALL, "Rus");
     vector <int> lst1, lst2, lst3;
     itc_pos_neg_separator_lst(lst, lst1, lst2, lst3);
-    cout << "Ïîëîæèòåëüíûå:" << '\t' << '\t' << "Îòðèöàòåëüíûå:" << endl;
-    cout << "Êîëè÷åñòâî ÷èñåë: " << lst1.size() << "," << '\t' << '\t' << "Êîëè÷åñòâî ÷èñåë: " << lst2.size() << endl;
-    cout << "Ìàêñèìàëüíàÿ öèôðà: " << itc_max_lst(lst1) << "," << '\t' << '\t' << "Ìàêñèìàëüíàÿ öèôðà: " << itc_max_lst(lst2) <<"," << endl;
-    cout << "Ìèíèìàëüíàÿ öèôðà: " << itc_min_lst(lst1) << "," << '\t' <<'\t' << "Ìèíèìàëüíàÿ öèôðà: " << itc_min_lst(lst2)<<"," << endl;
-    cout << "Ñóììà ÷èñåë: " << itc_sumlst(lst1) << "," << '\t' <<'\t' << "Ñóììà ÷èñåë: "  << itc_sumlst(lst2)<<"," << endl;
-    cout << "Ñðåäíåå çíà÷åíèå: " << (itc_sumlst(lst1) / lst1.size()) << "," << '\t' << '\t' << "Ñðåäíåå çíà÷åíèå: " << (itc_sumlst(lst2) / lst2.size()) << "," << endl;
+    cout << "ÃÃ®Ã«Ã®Ã¦Ã¨Ã²Ã¥Ã«Ã¼Ã­Ã»Ã¥:" << '\t' << '\t' << "ÃŽÃ²Ã°Ã¨Ã¶Ã Ã²Ã¥Ã«Ã¼Ã­Ã»Ã¥:" << endl;
+    cout << "ÃŠÃ®Ã«Ã¨Ã·Ã¥Ã±Ã²Ã¢Ã® Ã·Ã¨Ã±Ã¥Ã«: " << lst1.size() << "," << '\t' << '\t' << "ÃŠÃ®Ã«Ã¨Ã·Ã¥Ã±Ã²Ã¢Ã® Ã·Ã¨Ã±Ã¥Ã«: " << lst2.size() << endl;
+    cout << "ÃŒÃ ÃªÃ±Ã¨Ã¬Ã Ã«Ã¼Ã­Ã Ã¿ Ã¶Ã¨Ã´Ã°Ã : " << itc_max_lst(lst1) << "," << '\t' << '\t' << "ÃŒÃ ÃªÃ±Ã¨Ã¬Ã Ã«Ã¼Ã­Ã Ã¿ Ã¶Ã¨Ã´Ã°Ã : " << itc_max_lst(lst2) <<"," << endl;
+    cout << "ÃŒÃ¨Ã­Ã¨Ã¬Ã Ã«Ã¼Ã­Ã Ã¿ Ã¶Ã¨Ã´Ã°Ã : " << itc_min_lst(lst1) << "," << '\t' <<'\t' << "ÃŒÃ¨Ã­Ã¨Ã¬Ã Ã«Ã¼Ã­Ã Ã¿ Ã¶Ã¨Ã´Ã°Ã : " << itc_min_lst(lst2)<<"," << endl;
+    cout << "Ã‘Ã³Ã¬Ã¬Ã  Ã·Ã¨Ã±Ã¥Ã«: " << itc_sumlst(lst1) << "," << '\t' <<'\t' << "Ã‘Ã³Ã¬Ã¬Ã  Ã·Ã¨Ã±Ã¥Ã«: "  << itc_sumlst(lst2)<<"," << endl;
+    cout << "Ã‘Ã°Ã¥Ã¤Ã­Ã¥Ã¥ Ã§Ã­Ã Ã·Ã¥Ã­Ã¨Ã¥: " << (itc_sumlst(lst1) / lst1.size()) << "," << '\t' << '\t' << "Ã‘Ã°Ã¥Ã¤Ã­Ã¥Ã¥ Ã§Ã­Ã Ã·Ã¥Ã­Ã¨Ã¥: " << (itc_sumlst(lst2) / lst2.size()) << "," << endl;
     cout << endl;
-    cout << "Êîëè÷åñòâî íóëåé: " << lst3.size() << endl;}
+    cout << "ÃŠÃ®Ã«Ã¨Ã·Ã¥Ã±Ã²Ã¢Ã® Ã­Ã³Ã«Ã¥Ã©: " << lst3.size() << endl;}
