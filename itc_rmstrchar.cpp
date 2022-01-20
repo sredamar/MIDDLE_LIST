e00adf3600d0cabef329bfd6ff9cb57796e9398e
sredamar <92477629+sredamar@users.noreply.github.com>
2022-01-20 11:21:00 +0300

    Update itc_rmstrchar.cpp

--- itc_rmstrchar.cpp
+++ itc_rmstrchar.cpp
@@ -1,7 +1,9 @@
 #include "middle_list.h"
 string itc_rmstrchar(string str, string l) {
+    if (itc_len(str) != 0) {
     string n = "";
     int t = itc_len(l);
         for (int i = 0; str[i] != '\0'; i++) if (itc_slice_str(str, i, i + t - 1) != l) n += str[i];
             else i += t - 1;
 return n;}
+}
