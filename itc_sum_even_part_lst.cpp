143086fb99da8beccf85e7167e3574892cf662cd
sredamar <92477629+sredamar@users.noreply.github.com>
2022-01-20 11:15:41 +0300

    Update itc_sum_even_part_lst.cpp

--- itc_sum_even_part_lst.cpp
+++ itc_sum_even_part_lst.cpp
@@ -4,4 +4,4 @@ long itc_sum_even_part_lst(const vector <int> &lst) {
     long sum = 0;
     for (int i = 0; i < lst.size(); i++) if (lst[i] % 2 == 0) sum += lst[i];
     return sum;}
-}
+return 0;}
