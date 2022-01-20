cc6ecf0cf2e51e91a47b7c4d3b12a0ea1b05b60d
sredamar <92477629+sredamar@users.noreply.github.com>
2022-01-20 18:15:37 +0300

    #addons

--- addons.cpp
+++ addons.cpp
@@ -1,21 +1,13 @@
 #include "middle_list.h"
 long long itc_len(string str) {
     long long kol = 0;
-    for (long long i = 0; str[i] != '\0'; i++) kol++;
-    return kol;}
-long long itc_min(long long a, long long b) {
-    if (a < b) return a;
-return b;}
-string itc_slice_str(string str, int st, int fin) {
-    string res = "";
-    for (int i = st; i <= itc_min(fin, itc_len(str) - 1); i++) res += str[i];
-    if (fin < st) return str;
-return res;}
-int itc_find_str(string str1, string str2){
-    long long len1 = itc_len(str1), len2 = itc_len(str2), kol = 0, i1 = 0;
-    while (i1 < len1){
-        if (str1[i1] == str2[0]) for (long long i2 = 0; i2 < len2; i2++) if (str1[i1 + i2] == str2[i2]) kol++;
-        if (kol == len2)
-            return i1;
-            i1++;}
-return -1;}
+    for (int i = 0; str[i] != '\0'; i++) kol++;
+return kol;}
+int itc_max_lst(const vector <int> &lst){
+    int max_num = -2147483648;
+    if (lst.size() > 0) for(int i =0; i < lst.size(); i++) if (lst[i] > max_num) max_num = lst[i];
+return max_num;}
+int itc_min_lst(const vector <int> &lst){
+    int min_num = 2147483648;
+    if (lst.size() > 0) for(int i =0; i < lst.size(); i++) if (lst[i] < min_num) min_num = lst[i];
+return min_num;}
