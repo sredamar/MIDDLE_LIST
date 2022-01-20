c865a6b1208a5268ca4b29fac88ef3f8cf76d45b
sredamar <92477629+sredamar@users.noreply.github.com>
2022-01-20 11:36:18 +0300

    Update itc_sumlst.cpp

--- itc_sumlst.cpp
+++ itc_sumlst.cpp
@@ -1,7 +1,7 @@
 #include "middle_list.h"
 long itc_sumlst(const vector <int> &lst) {
-    if (lst.size() != 0) {
     long sum = 0;
+    if (lst.size() != 0) {
     for (int i = 0; i < lst.size(); i++) sum += lst[i];
     return sum;}
 return 0;}
