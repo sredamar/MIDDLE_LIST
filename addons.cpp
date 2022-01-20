c065411b60fc3ca4a4d3d3e92b9b62d6b5ebfe80
sredamar <92477629+sredamar@users.noreply.github.com>
2022-01-20 12:12:02 +0300

    Update addons.cpp

--- addons.cpp
+++ addons.cpp
@@ -12,9 +12,9 @@ string itc_slice_str(string str, int st, int fin) {
     if (fin < st) return str;
 return res;}
 int itc_find_str(string str1, string str2){
-    long long len1 = itc_len(str1), len2 = itc_len(str2), kol = 0, i1 = 0, i2 = 0;
+    long long len1 = itc_len(str1), len2 = itc_len(str2), kol = 0, i1 = 0;
     while (i1 < len1){
-        if (str1[i1] == str2[0]) for (i2; i2 < len2; i2++) if (str1[i1 + i2] == str2[i2]) kol++;
+        if (str1[i1] == str2[0]) for (long long i2 = 0; i2 < len2; i2++) if (str1[i1 + i2] == str2[i2]) kol++;
         if (kol == len2)
             return i1;
             i1++;}
