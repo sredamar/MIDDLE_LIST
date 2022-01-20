4fc81e9bb74620d8693ff24f2122a078e4b5632f
sredamar <92477629+sredamar@users.noreply.github.com>
2022-01-20 11:24:41 +0300

    Update itc_odd_even_separator_lst.cpp

--- itc_odd_even_separator_lst.cpp
+++ itc_odd_even_separator_lst.cpp
@@ -1,6 +1,8 @@
 #include "middle_list.h"
 void itc_odd_even_separator_lst(const vector <int> &lst, vector <int> &lst1, vector <int> &lst2) {
+    if (lst.size() != 0) {
     for (int i = 0; i < lst.size(); i++) {
         if (lst[i] % 2 == 0) lst1.push_back(lst[i]);
         else lst2.push_back(lst[i]);}
+}
 }
