97354f3696c3822309d7dd8ffb356cdc62991c55
sredamar <92477629+sredamar@users.noreply.github.com>
2022-01-20 18:17:40 +0300

    #9

--- itc_pos_neg_separator_lst.cpp
+++ itc_pos_neg_separator_lst.cpp
@@ -1,9 +1,10 @@
 #include "middle_list.h"
 void itc_pos_neg_separator_lst(const vector <int> &lst, vector <int> &lst1, vector <int> &lst2, vector <int> &lst3) {
-    if (lst.size() != 0) {
-    for (int i = 0; i < lst.size(); i++) {
+    if(lst.size() > 0){
+        for(long long i =0; i < lst.size(); i++){
         if (lst[i] < 0) lst1.push_back(lst[i]);
         else if (lst[i] == 0) lst2.push_back(lst[i]);
-        else lst3.push_back(lst[i]);}
-}
-}
+        else lst3.push_back(lst[i]);
+        }
+    }
+    }
