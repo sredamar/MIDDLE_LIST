869b1b272892b1bd1c796199f61e3593beb57c02
sredamar <92477629+sredamar@users.noreply.github.com>
2022-01-20 18:18:59 +0300

    #1

--- itc_strlist.cpp
+++ itc_strlist.cpp
@@ -1,5 +1,5 @@
 #include "middle_list.h"
 vector <char> itc_strtlist(string str){
-    vector <char> mass(itc_len(str));
-    for(int i = 0; i < itc_len(str); i++) mass[i] = str[i];
-return mass;}
+    vector<char> n;
+    if (itc_len(str) > 0) for(long long i =0; str[i] != '\0'; i++) n.push_back(str[i]);
+return n;}
